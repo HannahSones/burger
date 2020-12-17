a27d527029ce3b0b2baa4ea7b878e3b585982354
Hannah Sones <70013364+HannahSones@users.noreply.github.com>
2020-12-17 19:21:16 +0000

    Creating burger database and adding seed data

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burgers_db;
+
+INSERT INTO burgers(burger_name, devoured)
+
+VALUES
+	("Cheeseburger", true), ("Hamburger", true), ("Chicken burger", false), ("Veggie burger", true), ("Quarterpounder burger", false);
