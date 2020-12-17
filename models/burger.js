bb89c96bc47188a701f855932ca45c4e7ceba30d
Hannah Sones <70013364+HannahSones@users.noreply.github.com>
2020-12-17 21:15:08 +0000

    Setting up code to call ORM functions

---
+++ models/burger.js
@@ -0,0 +1,9 @@
+const orm = require("../config/orm.js");
+
+// const burger = {
+//     all: () => orm.all("burgers"),
+//     create: (cols, vals) => orm.create("burgers", cols, vals),
+//     update: (objColVals, condition) => orm.update("burgers", objColVals, condition)
+// };
+
+module.exports = burger;
